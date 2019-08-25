64c672b67ca9e9e6b8555e2b78654a0cadbca11d
ph03nix-x <ph03nix-x@outlook.com>
2019-08-25 13:05:55 +0300

    Add Charge Current Add Discharge Current Refactoring

--- app/src/main/java/com/ph03nix_x/capacityinfo/MainActivity.kt
+++ app/src/main/java/com/ph03nix_x/capacityinfo/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.ph03nix_x.capacityinfo
 
-import android.content.Context
-import android.content.Intent
-import android.content.SharedPreferences
+import android.content.*
 import android.net.Uri
 import android.os.AsyncTask
+import android.os.BatteryManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -12,26 +11,28 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.RelativeLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import java.io.BufferedReader
 import java.io.File
 import java.io.FileInputStream
 import java.io.InputStreamReader
+import java.lang.Exception
 import java.text.DecimalFormat
 
 const val chargeFull = "/sys/class/power_supply/battery/charge_full"
 const val chargeFullDesign = "/sys/class/power_supply/battery/charge_full_design"
-const val chargeCounter = "/sys/class/power_supply/battery/charge_counter"
-const val batteryLevelPath = "/sys/class/power_supply/battery/capacity"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var capacityDesign: TextView
     private lateinit var residualCapacity: TextView
     private lateinit var currentCapacity: TextView
+    private lateinit var chargingCurrent: TextView
     private lateinit var batteryWear: TextView
     private lateinit var pref: SharedPreferences
     private lateinit var relativeMain: RelativeLayout
-    var asyncTask: AsyncTask<Void, Void, Unit>? = null
+    private lateinit var batteryManager: BatteryManager
+    private var asyncTask: AsyncTask<Void, Void, Unit>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -46,7 +47,10 @@ class MainActivity : AppCompatActivity() {
         capacityDesign = findViewById(R.id.capacity_design)
         currentCapacity = findViewById(R.id.current_capacity)
         residualCapacity = findViewById(R.id.residual_capacity)
+        chargingCurrent = findViewById(R.id.charging_current)
         batteryWear = findViewById(R.id.battery_wear)
+
+        batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
 
         if(pref.getBoolean("dark_mode", false)) relativeMain.setBackgroundColor(ContextCompat.getColor(this, R.color.dark))
     }
@@ -77,19 +81,59 @@ class MainActivity : AppCompatActivity() {
                         }
                     }
 
-                    if (File(chargeCounter).exists() && File(batteryLevelPath).exists() && getCurrentCapacity() > 0) {
+                    if (getCurrentCapacity() > 0) {
 
                         if (currentCapacity.visibility == View.GONE) currentCapacity.visibility = View.VISIBLE
 
                         runOnUiThread {
 
-                            currentCapacity.text = getString(R.string.current_capacity, getDecimalFormat(getCurrentCapacity()), "${getBatteryLevel()}%")
+                                currentCapacity.text = getString(R.string.current_capacity, getDecimalFormat(getCurrentCapacity()), "${getBatteryLevel()}%")
                         }
                     }
 
                     else {
 
                         if (currentCapacity.visibility == View.VISIBLE) currentCapacity.visibility = View.GONE
+                    }
+
+                    val intentFilter = IntentFilter()
+
+                    intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED)
+
+                    intentFilter.addAction(Intent.ACTION_POWER_CONNECTED)
+
+                    intentFilter.addAction(Intent.ACTION_POWER_DISCONNECTED)
+
+                    val batteryStatus = registerReceiver(null, intentFilter)
+
+                    val status = batteryStatus?.getIntExtra(BatteryManager.EXTRA_STATUS, -1)
+
+                    val plugged = batteryStatus?.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0)
+
+                    runOnUiThread {
+
+                        Toast.makeText(this, status.toString(), Toast.LENGTH_LONG).show()
+                    }
+
+                    if(status != BatteryManager.BATTERY_STATUS_DISCHARGING && plugged == BatteryManager.BATTERY_PLUGGED_AC or BatteryManager.BATTERY_PLUGGED_USB) {
+
+                        if(chargingCurrent.visibility == View.GONE) chargingCurrent.visibility = View.VISIBLE
+
+                        runOnUiThread {
+
+                            chargingCurrent.text = getString(R.string.charging_current, getChargingCurrent().toString())
+                        }
+
+                    }
+
+                    else {
+
+                        if(chargingCurrent.visibility == View.GONE) chargingCurrent.visibility = View.VISIBLE
+
+                        runOnUiThread {
+
+                            chargingCurrent.text = getString(R.string.discharge_current, getChargingCurrent().toString())
+                        }
                     }
 
                     Thread.sleep(5000)
@@ -100,13 +144,15 @@ class MainActivity : AppCompatActivity() {
 
         else {
 
-            capacityDesign.text = getString(R.string.capacity_design, "0")
+            capacityDesign.text = getString(R.string.capacity_design, getDesignCapacity().toString())
 
             residualCapacity.text = getString(R.string.residual_capacity, "0", "0%")
 
             batteryWear.text = getString(R.string.battery_wear, "0%")
 
             if (currentCapacity.visibility == View.VISIBLE) currentCapacity.visibility = View.GONE
+
+            if(chargingCurrent.visibility == View.VISIBLE) chargingCurrent.visibility = View.GONE
         }
 
     }
@@ -205,43 +251,26 @@ class MainActivity : AppCompatActivity() {
 
     private fun getCurrentCapacity(): Double {
 
-        var capacity = 0.0
+        var currentCapacity = 0.0
 
-        val file = File(chargeCounter)
+        try {
 
-            if (file.exists()) {
+            currentCapacity = batteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CHARGE_COUNTER).toDouble()
 
-                val input = FileInputStream(file)
-                val isr = InputStreamReader(input)
-                val buff = BufferedReader(isr)
+            if (currentCapacity < 0) currentCapacity /= -1
 
-                capacity = buff.readLine().toDouble()
-
-                input.close()
-            }
-
-            return capacity
-    }
-
-    private fun getBatteryLevel(): String {
-
-        var batteryLevel = "0"
-
-        val file = File(batteryLevelPath)
-
-        if (file.exists()) {
-
-            val input = FileInputStream(file)
-            val isr = InputStreamReader(input)
-            val buff = BufferedReader(isr)
-
-            batteryLevel = buff.readLine()
-
-            input.close()
+            if (currentCapacity >= 100000) currentCapacity /= 1000
         }
 
-        return batteryLevel
+        catch (e: Exception) {
+
+            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show()
+        }
+
+        return currentCapacity
     }
+
+    private fun getBatteryLevel() = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
 
     private fun getResidualCapacity(): Double {
 
@@ -261,6 +290,17 @@ class MainActivity : AppCompatActivity() {
             }
 
             return capacity
+    }
+
+    private fun getChargingCurrent(): Int {
+
+        var chargingCurrent = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW)
+
+        if(chargingCurrent < 0) chargingCurrent /= -1
+
+        if(chargingCurrent >= 100000) chargingCurrent /= 1000
+
+        return chargingCurrent
     }
 
     private fun getBatteryWear(capacityDesign: Double, capacity: Double) = "${DecimalFormat("#.#").format(100 - ((capacity / capacityDesign) * 100))}%"
