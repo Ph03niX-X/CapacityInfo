6590199c4d4140070d98cda485ebcbc88b9cd230
ph03nix-x <ph03nix-x@outlook.com>
2019-11-18 13:03:58 +0200

    SettingsInterface

--- app/src/main/java/com/ph03nix_x/capacityinfo/fragments/SettingsFragment.kt
+++ app/src/main/java/com/ph03nix_x/capacityinfo/fragments/SettingsFragment.kt
@@ -1,36 +1,22 @@
 package com.ph03nix_x.capacityinfo.fragments
 
-import android.app.NotificationManager
 import android.content.*
-import android.os.BatteryManager
 import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
-import android.view.LayoutInflater
-import androidx.annotation.RequiresApi
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 import androidx.preference.SwitchPreferenceCompat
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.ph03nix_x.capacityinfo.Preferences
 import com.ph03nix_x.capacityinfo.R
 import com.ph03nix_x.capacityinfo.activity.MainActivity
 import com.ph03nix_x.capacityinfo.services.CapacityInfoService
-import android.widget.*
 import androidx.appcompat.app.AppCompatDelegate
+import com.ph03nix_x.capacityinfo.SettingsInterface
 import com.ph03nix_x.capacityinfo.services.ServiceInterface
 import com.ph03nix_x.capacityinfo.activity.SettingsActivity
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
-class SettingsFragment : PreferenceFragmentCompat(), ServiceInterface {
-
-    private var jobUpdateNotification: Job? = null
-
-    private var progressSeekBar = -1
+class SettingsFragment : PreferenceFragmentCompat(), ServiceInterface, SettingsInterface {
 
     private lateinit var pref: SharedPreferences
 
@@ -143,7 +129,7 @@ class SettingsFragment : PreferenceFragmentCompat(), ServiceInterface {
 
         showStopService?.setOnPreferenceChangeListener { _, _ ->
 
-            if(CapacityInfoService.instance != null) updateNotification()
+            if(CapacityInfoService.instance != null) updateNotification(requireContext())
 
             return@setOnPreferenceChangeListener true
         }
@@ -152,21 +138,21 @@ class SettingsFragment : PreferenceFragmentCompat(), ServiceInterface {
 
             serviceHours?.isEnabled = newValue as Boolean
 
-            if(CapacityInfoService.instance != null) updateNotification()
+            if(CapacityInfoService.instance != null) updateNotification(requireContext())
 
             return@setOnPreferenceChangeListener true
         }
 
         serviceHours?.setOnPreferenceChangeListener { _, _ ->
 
-            if(CapacityInfoService.instance != null) updateNotification()
+            if(CapacityInfoService.instance != null) updateNotification(requireContext())
 
             return@setOnPreferenceChangeListener true
         }
 
         showCapacityAddedInNotification?.setOnPreferenceChangeListener { _, _ ->
 
-            if(CapacityInfoService.instance != null) updateNotification()
+            if(CapacityInfoService.instance != null) updateNotification(requireContext())
 
             return@setOnPreferenceChangeListener true
         }
@@ -176,21 +162,21 @@ class SettingsFragment : PreferenceFragmentCompat(), ServiceInterface {
             showLastChargeTimeInNotification?.isEnabled = newValue as Boolean
             notificationRefreshRate?.isEnabled = newValue
 
-            if(CapacityInfoService.instance != null) updateNotification()
+            if(CapacityInfoService.instance != null) updateNotification(requireContext())
 
             return@setOnPreferenceChangeListener true
         }
 
         showLastChargeTimeInNotification?.setOnPreferenceChangeListener { _, _ ->
 
-            if(CapacityInfoService.instance != null) updateNotification()
+            if(CapacityInfoService.instance != null) updateNotification(requireContext())
 
             return@setOnPreferenceChangeListener true
         }
 
         showCapacityAddedLastChargeTimeInNotification?.setOnPreferenceChangeListener { _, _ ->
 
-            if(CapacityInfoService.instance != null) updateNotification()
+            if(CapacityInfoService.instance != null) updateNotification(requireContext())
 
             return@setOnPreferenceChangeListener true
         }
@@ -199,7 +185,7 @@ class SettingsFragment : PreferenceFragmentCompat(), ServiceInterface {
 
             openNotificationCategorySettings?.setOnPreferenceClickListener {
 
-                openNotificationCategorySettings()
+                openNotificationCategorySettings(requireContext())
 
                 return@setOnPreferenceClickListener true
             }
@@ -207,7 +193,7 @@ class SettingsFragment : PreferenceFragmentCompat(), ServiceInterface {
         else openNotificationCategorySettings?.isVisible = false
 
         notificationRefreshRate?.setOnPreferenceClickListener {
-            notificationRefreshRateDialog()
+            notificationRefreshRateDialog(requireContext(), pref)
             return@setOnPreferenceClickListener true
         }
 
@@ -264,7 +250,7 @@ class SettingsFragment : PreferenceFragmentCompat(), ServiceInterface {
         temperatureInFahrenheit?.setOnPreferenceChangeListener { _, _ ->
 
             if(pref.getBoolean(Preferences.IsEnableService.prefKey, true) && CapacityInfoService.instance != null)
-                updateNotification()
+                updateNotification(requireContext())
 
             return@setOnPreferenceChangeListener true
         }
@@ -272,14 +258,14 @@ class SettingsFragment : PreferenceFragmentCompat(), ServiceInterface {
         voltageInMv?.setOnPreferenceChangeListener { _, _ ->
 
             if(pref.getBoolean(Preferences.IsEnableService.prefKey, true) && CapacityInfoService.instance != null)
-                updateNotification()
+                updateNotification(requireContext())
 
             return@setOnPreferenceChangeListener true
         }
 
         changeDesignCapacity?.setOnPreferenceClickListener {
 
-            changeDesignCapacity()
+            changeDesignCapacity(requireContext(), pref)
 
             return@setOnPreferenceClickListener true
         }
@@ -311,199 +297,5 @@ class SettingsFragment : PreferenceFragmentCompat(), ServiceInterface {
         }
     }
 
-    private fun updateNotification() {
 
-        if(jobUpdateNotification == null)
-            jobUpdateNotification = GlobalScope.launch {
-
-                delay(1000)
-                val intent = requireContext().registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
-                if(intent?.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0) == 0) CapacityInfoService.instance?.updateNotification(requireContext())
-                jobUpdateNotification = null
-            }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun openNotificationCategorySettings() {
-
-        val notificationManager = context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        val notificationChannel = notificationManager.getNotificationChannel("service_channel")
-
-        val intent = Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS).apply {
-
-            putExtra(Settings.EXTRA_CHANNEL_ID, notificationChannel.id)
-
-            putExtra(Settings.EXTRA_APP_PACKAGE, context?.packageName)
-
-        }
-
-        startActivity(intent)
-    }
-
-    private fun notificationRefreshRateDialog() {
-
-        val dialog = MaterialAlertDialogBuilder(requireActivity())
-
-        val view = LayoutInflater.from(requireContext()).inflate(R.layout.notification_refresh_rate, null)
-
-        dialog.setView(view)
-
-        val notificationRefreshRate = view.findViewById<TextView>(R.id.notification_refresh_rate_textView)
-
-        val notificationRefreshRateSeekBar = view.findViewById<SeekBar>(R.id.notification_refresh_rate_seekBar)
-
-        val notificationRefreshRatePref = pref.getLong(Preferences.NotificationRefreshRate.prefKey,40)
-
-        setProgress(notificationRefreshRatePref, notificationRefreshRateSeekBar)
-
-        getNotificationRefreshRateTime(notificationRefreshRatePref, notificationRefreshRate, notificationRefreshRateSeekBar)
-
-        notificationRefreshRateSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
-
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) { progressChanged(progress, notificationRefreshRate); progressSeekBar = progress }
-
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
-        })
-
-        dialogApply(dialog)
-    }
-
-    private fun setProgress(notificationRefreshRate: Long, notificationRefreshRateSeekBar: SeekBar) {
-
-        when(notificationRefreshRate) {
-
-            5.toLong() -> notificationRefreshRateSeekBar.progress = 0
-            10.toLong() -> notificationRefreshRateSeekBar.progress = 9
-            15.toLong() -> notificationRefreshRateSeekBar.progress = 17
-            20.toLong() -> notificationRefreshRateSeekBar.progress = 25
-            25.toLong() -> notificationRefreshRateSeekBar.progress = 33
-            30.toLong() -> notificationRefreshRateSeekBar.progress = 41
-            35.toLong() -> notificationRefreshRateSeekBar.progress = 49
-            40.toLong() -> notificationRefreshRateSeekBar.progress = 57
-            45.toLong() -> notificationRefreshRateSeekBar.progress = 65
-            50.toLong() -> notificationRefreshRateSeekBar.progress = 73
-            55.toLong() -> notificationRefreshRateSeekBar.progress = 81
-            60.toLong() -> notificationRefreshRateSeekBar.progress = 100
-        }
-    }
-
-    private fun getNotificationRefreshRateTime(notificationRefreshRatePref: Long, notificationRefreshRate: TextView, notificationRefreshRateSeekBar: SeekBar) {
-
-        val sleepArray = arrayOf<Long>(5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60)
-
-        if(notificationRefreshRatePref !in sleepArray) {
-
-            notificationRefreshRateSeekBar.progress = 57
-
-            pref.edit().putLong(Preferences.NotificationRefreshRate.prefKey, 40).apply()
-        }
-
-        notificationRefreshRate.text = getString(if(notificationRefreshRatePref != 60.toLong()) R.string.seconds
-        else R.string.minute, if(notificationRefreshRatePref < 60) notificationRefreshRatePref.toString() else "1")
-    }
-
-    private fun progressChanged(progress: Int, notificationRefreshRate: TextView) {
-
-        when(progress) {
-
-            in 0..8 -> notificationRefreshRate.text = getString(R.string.seconds, "5")
-
-            in 9..16 -> notificationRefreshRate.text = getString(R.string.seconds, "10")
-
-            in 17..24 -> notificationRefreshRate.text = getString(R.string.seconds, "15")
-
-            in 25..32 -> notificationRefreshRate.text = getString(R.string.seconds, "20")
-
-            in 33..40 -> notificationRefreshRate.text = getString(R.string.seconds, "25")
-
-            in 41..48 -> notificationRefreshRate.text = getString(R.string.seconds, "30")
-
-            in 49..56 -> notificationRefreshRate.text = getString(R.string.seconds, "35")
-
-            in 57..64 -> notificationRefreshRate.text = getString(R.string.seconds, "40")
-
-            in 65..72 -> notificationRefreshRate.text = getString(R.string.seconds, "45")
-
-            in 73..80 -> notificationRefreshRate.text = getString(R.string.seconds, "50")
-
-            in 81..88 -> notificationRefreshRate.text = getString(R.string.seconds, "55")
-
-            in 89..100 -> notificationRefreshRate.text = getString(R.string.minute, "1")
-        }
-    }
-
-    private fun dialogApply(dialog: MaterialAlertDialogBuilder) {
-
-        dialog.apply {
-
-            setTitle(getString(R.string.notification_refresh_rate))
-
-            setPositiveButton(getString(R.string.apply)) { _, _ ->
-
-                when(progressSeekBar) {
-
-                    in 0..8 -> pref.edit().putLong(Preferences.NotificationRefreshRate.prefKey, 5).apply()
-
-                    in 9..16 -> pref.edit().putLong(Preferences.NotificationRefreshRate.prefKey, 10).apply()
-
-                    in 17..24 -> pref.edit().putLong(Preferences.NotificationRefreshRate.prefKey, 15).apply()
-
-                    in 25..32 -> pref.edit().putLong(Preferences.NotificationRefreshRate.prefKey, 20).apply()
-
-                    in 33..40 -> pref.edit().putLong(Preferences.NotificationRefreshRate.prefKey, 25).apply()
-
-                    in 41..48 -> pref.edit().putLong(Preferences.NotificationRefreshRate.prefKey, 30).apply()
-
-                    in 49..56 -> pref.edit().putLong(Preferences.NotificationRefreshRate.prefKey, 35).apply()
-
-                    in 57..64 -> pref.edit().putLong(Preferences.NotificationRefreshRate.prefKey, 40).apply()
-
-                    in 65..72 -> pref.edit().putLong(Preferences.NotificationRefreshRate.prefKey, 45).apply()
-
-                    in 73..80 -> pref.edit().putLong(Preferences.NotificationRefreshRate.prefKey, 50).apply()
-
-                    in 81..88 -> pref.edit().putLong(Preferences.NotificationRefreshRate.prefKey, 55).apply()
-
-                    in 89..100 -> pref.edit().putLong(Preferences.NotificationRefreshRate.prefKey, 60).apply()
-                }
-            }
-
-            setNegativeButton(getString(android.R.string.cancel)) { d, _ -> d.dismiss() }
-
-            show()
-        }
-    }
-
-    private fun changeDesignCapacity() {
-
-        val dialog = MaterialAlertDialogBuilder(requireContext())
-
-        val view = LayoutInflater.from(requireContext()).inflate(R.layout.change_design_capacity, null)
-
-        dialog.setView(view)
-
-        val changeDesignCapacity = view.findViewById<EditText>(R.id.change_design_capacity_edit)
-
-        changeDesignCapacity.setText(if(pref.getInt(Preferences.DesignCapacity.prefKey, 0) >= 0) pref.getInt(
-            Preferences.DesignCapacity.prefKey, 0).toString()
-
-        else (pref.getInt(Preferences.DesignCapacity.prefKey, 0) / -1).toString())
-
-        dialog.setPositiveButton(getString(R.string.change)) { _, _ ->
-
-            if(changeDesignCapacity.text.isNotEmpty()) pref.edit().putInt(Preferences.DesignCapacity.prefKey, changeDesignCapacity.text.toString().toInt()).apply()
-
-            CapacityInfoService.instance?.sleepTime = pref.getLong(Preferences.NotificationRefreshRate.prefKey, 40)
-
-            if(pref.getBoolean(Preferences.IsEnableService.prefKey, true) && CapacityInfoService.instance != null)
-                updateNotification()
-        }
-
-        dialog.setNegativeButton(android.R.string.cancel) { d, _ -> d.dismiss() }
-
-        dialog.show()
-    }
 }
