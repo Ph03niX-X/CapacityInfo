efa8175a00e1ff28ead2cb8a229de2d769762c45
ph03nix-x <ph03nix-x@outlook.com>
2020-01-12 18:35:57 +0200

    Changed Dialog Message in FAQ

--- app/src/main/java/com/ph03nix_x/capacityinfo/activity/MainActivity.kt
+++ app/src/main/java/com/ph03nix_x/capacityinfo/activity/MainActivity.kt
@@ -91,7 +91,8 @@ class MainActivity : AppCompatActivity(), ServiceInterface, BatteryInfoInterface
 
                 setIcon(R.drawable.ic_help_outline_dialog_24dp)
                 setTitle(getString(R.string.faq))
-                setMessage(getString(R.string.faq_how_does_the_app_work) + getString(R.string.faq_add_device_support))
+                setMessage(getString(R.string.faq_how_does_the_app_work) + getString(R.string.faq_capacity_added)
+                        + getString(R.string.faq_add_device_support))
                 setPositiveButton(android.R.string.ok) { d, _ -> d.dismiss() }
                 show()
             }
