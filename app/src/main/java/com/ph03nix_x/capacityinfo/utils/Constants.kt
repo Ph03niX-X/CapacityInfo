ae490cc340627d1df2ca81b2183a0f5f6965760c
ph03nix-x <ph03nix-x@outlook.com>
2020-01-18 12:04:44 +0200

    Changed help with translation link

--- app/src/main/java/com/ph03nix_x/capacityinfo/utils/Constants.kt
+++ app/src/main/java/com/ph03nix_x/capacityinfo/utils/Constants.kt
@@ -6,7 +6,7 @@ object Constants {
     const val designerLink = "https://t.me/F0x1d"
     const val romanianTranslationLink = "https://github.com/ygorigor"
     const val belorussianTranslationLink = "https://t.me/DrCyanogen"
-    const val helpWithTranslationLink = "https://drive.google.com/open?id=1DoBUA5wye-vGw8Me6b-7Y44iXfpBSu91"
+    const val helpWithTranslationLink = "https://drive.google.com/drive/folders/1DoBUA5wye-vGw8Me6b-7Y44iXfpBSu91"
     const val telegramLink = "https://t.me/Ph03niX_X"
     const val exportSettingsRequestCode = 0
     const val importSettingsRequestCode = 1
