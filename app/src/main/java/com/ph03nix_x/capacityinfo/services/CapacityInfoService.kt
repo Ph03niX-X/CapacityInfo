03f1e7632804bbca615628589a124a94d8b2754d
ph03nix-x <ph03nix-x@outlook.com>
2020-03-08 07:10:42 +0200

    Fix

--- app/src/main/java/com/ph03nix_x/capacityinfo/services/CapacityInfoService.kt
+++ app/src/main/java/com/ph03nix_x/capacityinfo/services/CapacityInfoService.kt
@@ -121,8 +121,8 @@ class CapacityInfoService : Service(), NotificationInterface, BatteryInfoInterfa
 
                         for(display in displayManager.displays)
                             if(display.state == Display.STATE_ON)
-                                delay(if(getCurrentCapacity(this@CapacityInfoService) > 0) 948 else 955)
-                            else delay(if(getCurrentCapacity(this@CapacityInfoService) > 0) 918 else 925)
+                                delay(if(getCurrentCapacity(this@CapacityInfoService) > 0) 949 else 956)
+                            else delay(if(getCurrentCapacity(this@CapacityInfoService) > 0) 919 else 926)
 
                         seconds++
                         updateNotification(this@CapacityInfoService)
