a7fafcc3014dd23a35ccda33ffe6ade173164001
ph03nix-x <ph03nix-x@outlook.com>
2020-04-27 10:29:04 +0300

    Revert "Changed link to "Help with translation""
    This reverts commit 87aca92f

--- app/src/main/java/com/ph03nix_x/capacityinfo/utils/Constants.kt
+++ app/src/main/java/com/ph03nix_x/capacityinfo/utils/Constants.kt
@@ -6,7 +6,7 @@ object Constants {
     const val DESIGNER_LINK = "https://t.me/F0x1d"
     const val ROMANIAN_TRANSLATION_LINK = "https://github.com/ygorigor"
     const val BELARUSIAN_TRANSLATION_LINK = "https://t.me/DrCyanogen"
-    const val HELP_WITH_TRANSLATION_LINK = "https://poeditor.com/join/project/oviNsUZg0c"
+    const val HELP_WITH_TRANSLATION_LINK = "https://drive.google.com/drive/folders/1DoBUA5wye-vGw8Me6b-7Y44iXfpBSu91"
     const val TELEGRAM_LINK = "https://t.me/Ph03niX_X"
     const val GOOGLE_PLAY_PACKAGE_NAME = "com.android.vending"
     const val EXPORT_SETTINGS_REQUEST_CODE = 0
