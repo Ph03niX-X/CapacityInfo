f4bbd3843f572f344fae38e41b97b1d7a5e5b735
ph03nix-x <ph03nix-x@outlook.com>
2020-05-04 08:41:04 +0300

    Remove "removeOldPref()"

--- app/src/main/java/com/ph03nix_x/capacityinfo/receivers/RestartServiceReceiver.kt
+++ app/src/main/java/com/ph03nix_x/capacityinfo/receivers/RestartServiceReceiver.kt
@@ -23,8 +23,6 @@ class RestartServiceReceiver : BroadcastReceiver(), ServiceInterface {
 
                 val pref = PreferenceManager.getDefaultSharedPreferences(context)
 
-                removeOldPref(context)
-
                 if(CapacityInfoService.instance == null && !isStartedService) {
 
                     isStartedService = true
@@ -43,52 +41,6 @@ class RestartServiceReceiver : BroadcastReceiver(), ServiceInterface {
                 else if(isEnabledOverlay(context) && OverlayService.instance == null
                     && pref.getBoolean(IS_ENABLED_OVERLAY, false))
                     context.startService(Intent(context, OverlayService::class.java))
-            }
-        }
-    }
-
-    @Deprecated("Will be removed soon")
-    private fun removeOldPref(context: Context) {
-
-        val pref = PreferenceManager.getDefaultSharedPreferences(context)
-
-        with(pref) {
-            
-            edit().apply {
-                
-                if(contains("always_show_notification")) remove("always_show_notification")
-
-                if(contains("show_last_charge_time")) remove("show_last_charge_time")
-
-                if(contains("dark_mode")) remove("dark_mode")
-
-                if(contains("enable_service")) remove("enable_service")
-
-                if(contains("is_show_information_while_charging")) remove("is_show_information_while_charging")
-
-                if(contains("is_show_information_during_discharge")) remove("is_show_information_during_discharge")
-
-                if(contains("notification_refresh_rate")) remove("notification_refresh_rate")
-
-                if(contains("charge_counter")) remove("charge_counter")
-
-                if(contains("is_show_debug")) remove("is_show_debug")
-
-                if(contains("is_enable_service")) remove("is_enable_service")
-
-                if(contains("migrated")) remove("migrated")
-
-                if(contains("is_show_charging_time_in_app")) remove("is_show_charging_time_in_app")
-
-                if(contains("is_show_last_charge_time_in_app")) remove("is_show_last_charge_time_in_app")
-
-                if(contains("is_show_capacity_added_in_app")) remove("is_show_capacity_added_in_app")
-
-                if(contains("is_show_capacity_added_last_charge_in_app")) remove("is_show_capacity_added_last_charge_in_app")
-
-                if(contains("is_show_capacity_added_last_charge_in_notification")) remove("is_show_capacity_added_last_charge_in_notification")
-
-                apply()
             }
         }
     }
