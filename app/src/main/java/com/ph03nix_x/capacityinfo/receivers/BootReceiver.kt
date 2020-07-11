111fe7150b3f0988d7908e23fdbca9352e7e0430
Ph03niX-X <Ph03niX-X@outlook.com>
2020-07-11 07:52:36 +0300

    Auto Start "Auto Backup Settings"

--- app/src/main/java/com/ph03nix_x/capacityinfo/receivers/BootReceiver.kt
+++ app/src/main/java/com/ph03nix_x/capacityinfo/receivers/BootReceiver.kt
@@ -1,12 +1,20 @@
 package com.ph03nix_x.capacityinfo.receivers
 
+import android.Manifest
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat.checkSelfPermission
+import androidx.preference.PreferenceManager
+import com.ph03nix_x.capacityinfo.R
 import com.ph03nix_x.capacityinfo.interfaces.OverlayInterface
 import com.ph03nix_x.capacityinfo.helpers.ServiceHelper
+import com.ph03nix_x.capacityinfo.services.AutoBackupSettingsJobService
 import com.ph03nix_x.capacityinfo.services.CapacityInfoService
 import com.ph03nix_x.capacityinfo.services.OverlayService
+import com.ph03nix_x.capacityinfo.utilities.Constants
+import com.ph03nix_x.capacityinfo.utilities.PreferencesKeys.IS_AUTO_BACKUP_SETTINGS
 
 class BootReceiver : BroadcastReceiver() {
 
@@ -16,6 +24,8 @@ class BootReceiver : BroadcastReceiver() {
 
             Intent.ACTION_BOOT_COMPLETED, "android.intent.action.QUICKBOOT_POWERON" -> {
 
+                val pref = PreferenceManager.getDefaultSharedPreferences(context)
+
                 if(CapacityInfoService.instance == null &&
                     !ServiceHelper.isStartedCapacityInfoService()) ServiceHelper.startService(
                     context, CapacityInfoService::class.java)
@@ -23,6 +33,15 @@ class BootReceiver : BroadcastReceiver() {
                 if(OverlayService.instance == null && OverlayInterface.isEnabledOverlay(context)
                     && !ServiceHelper.isStartedOverlayService())
                     ServiceHelper.startService(context, OverlayService::class.java)
+
+                if(pref.getBoolean(IS_AUTO_BACKUP_SETTINGS, context.resources.getBoolean(
+                        R.bool.is_auto_backup_settings)) && checkSelfPermission(context,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_GRANTED && checkSelfPermission(context,
+                        Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_GRANTED)
+                    ServiceHelper.jobSchedule(context, AutoBackupSettingsJobService::class.java,
+                        Constants.AUTO_BACKUP_SETTINGS_JOB_ID, 1 * 60 * 60 * 1000 /* 1 hour */)
             }
         }
     }
--- app/src/main/java/com/ph03nix_x/capacityinfo/receivers/UpdateApplicationReceiver.kt
+++ app/src/main/java/com/ph03nix_x/capacityinfo/receivers/UpdateApplicationReceiver.kt
@@ -1,14 +1,20 @@
 package com.ph03nix_x.capacityinfo.receivers
 
+import android.Manifest
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.preference.PreferenceManager
 import com.ph03nix_x.capacityinfo.R
 import com.ph03nix_x.capacityinfo.interfaces.OverlayInterface
 import com.ph03nix_x.capacityinfo.helpers.ServiceHelper
+import com.ph03nix_x.capacityinfo.services.AutoBackupSettingsJobService
 import com.ph03nix_x.capacityinfo.services.CapacityInfoService
 import com.ph03nix_x.capacityinfo.services.OverlayService
+import com.ph03nix_x.capacityinfo.utilities.Constants
+import com.ph03nix_x.capacityinfo.utilities.PreferencesKeys.IS_AUTO_BACKUP_SETTINGS
 import com.ph03nix_x.capacityinfo.utilities.PreferencesKeys.IS_ENABLED_DEBUG_OPTIONS
 import com.ph03nix_x.capacityinfo.utilities.PreferencesKeys.TEXT_FONT
 import com.ph03nix_x.capacityinfo.utilities.PreferencesKeys.TEXT_SIZE
@@ -22,6 +28,8 @@ class UpdateApplicationReceiver : BroadcastReceiver() {
 
             Intent.ACTION_MY_PACKAGE_REPLACED -> {
 
+                val pref = PreferenceManager.getDefaultSharedPreferences(context)
+
                 migratedPrefs(context)
 
                 if(CapacityInfoService.instance == null &&
@@ -31,6 +39,15 @@ class UpdateApplicationReceiver : BroadcastReceiver() {
                 if(OverlayService.instance == null && OverlayInterface.isEnabledOverlay(context)
                     && !ServiceHelper.isStartedOverlayService())
                     ServiceHelper.startService(context, OverlayService::class.java)
+
+                if(pref.getBoolean(IS_AUTO_BACKUP_SETTINGS, context.resources.getBoolean(
+                        R.bool.is_auto_backup_settings)) && checkSelfPermission(context,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_GRANTED && checkSelfPermission(context,
+                        Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_GRANTED)
+                    ServiceHelper.jobSchedule(context, AutoBackupSettingsJobService::class.java,
+                        Constants.AUTO_BACKUP_SETTINGS_JOB_ID, 1 * 60 * 60 * 1000 /* 1 hour */)
             }
         }
     }
