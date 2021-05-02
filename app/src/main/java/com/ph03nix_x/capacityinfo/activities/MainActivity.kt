dfe532c6f0d222f1b17add08f0c3db8aec5e1e42
Ph03niX-X <Ph03niX-X@outlook.com>
2021-05-02 21:19:29 +0300

    [FAQ] With each charge, the battery wear changes, then increases, then decreases

--- app/src/main/java/com/ph03nix_x/capacityinfo/activities/MainActivity.kt
+++ app/src/main/java/com/ph03nix_x/capacityinfo/activities/MainActivity.kt
@@ -535,6 +535,7 @@ class MainActivity : AppCompatActivity(), BatteryInfoInterface, SettingsInterfac
                         + getString(R.string.faq_residual_capacity_is_higher)
                         + getString(R.string.faq_battery_wear_changes_when_charger_is_disconnected)
                         + getString(R.string.faq_battery_wear_not_change)
+                        + getString(R.string.faq_with_each_charge_battery_wear_changes)
                         + getString(R.string.faq_where_does_the_app_get_the_number_of_cycles_android)
                         + getString(R.string.faq_not_displayed_number_of_cycles_android)
                         + getString(R.string.faq_add_device_support))
