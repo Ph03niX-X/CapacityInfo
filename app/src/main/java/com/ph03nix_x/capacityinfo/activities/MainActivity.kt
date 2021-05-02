4c1c630fa1481044763eb992fcc00990c8ef5b48
Ph03niX-X <Ph03niX-X@outlook.com>
2021-05-02 21:14:07 +0300

    [FAQ] Battery wear changes when the charger is disconnected

--- app/src/main/java/com/ph03nix_x/capacityinfo/activities/MainActivity.kt
+++ app/src/main/java/com/ph03nix_x/capacityinfo/activities/MainActivity.kt
@@ -533,6 +533,7 @@ class MainActivity : AppCompatActivity(), BatteryInfoInterface, SettingsInterfac
                         + getString(R.string.faq_i_have_everything_in_zeros)
                         + getString(R.string.faq_units) + getString(R.string.faq_current_capacity)
                         + getString(R.string.faq_residual_capacity_is_higher)
+                        + getString(R.string.faq_battery_wear_changes_when_charger_is_disconnected)
                         + getString(R.string.faq_battery_wear_not_change)
                         + getString(R.string.faq_where_does_the_app_get_the_number_of_cycles_android)
                         + getString(R.string.faq_not_displayed_number_of_cycles_android)
