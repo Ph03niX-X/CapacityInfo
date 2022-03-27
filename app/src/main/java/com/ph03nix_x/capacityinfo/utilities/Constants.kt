e8d2590828c4cf119bb474afe527fff05c788474
Ph03niX-X <Ph03niX-X@outlook.com>
2022-03-27 06:32:33 +0300

    Change Ukrainian translation

--- app/src/main/java/com/ph03nix_x/capacityinfo/utilities/Constants.kt
+++ app/src/main/java/com/ph03nix_x/capacityinfo/utilities/Constants.kt
@@ -7,7 +7,7 @@ object Constants {
     const val GOOGLE_PLAY_APP_LINK = "https://play.google.com/store/apps/details?id=com.ph03nix_x.capacityinfo"
     const val DONATE_LINK = "https://www.donationalerts.com/c/ph03nix_x"
     const val GITHUB_LINK = "https://github.com/Ph03niX-X/CapacityInfo"
-    const val UKRAINIAN_TRANSLATION_LINK = "https://t.me/Erdar2K21"
+    const val UKRAINIAN_TRANSLATION_LINK = "https://t.me/Erdar2K22"
     const val TELEGRAM_DEVELOPER_LINK = "https://t.me/Ph03niX_X"
     const val DONT_KILL_MY_APP_LINK = "https://dontkillmyapp.com"
     const val GOOGLE_PLAY_PACKAGE_NAME = "com.android.vending"
