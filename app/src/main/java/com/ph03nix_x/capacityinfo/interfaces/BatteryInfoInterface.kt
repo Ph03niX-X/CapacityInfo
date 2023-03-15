9be4c7aa39f3cabc50159dd6c38b711d6d97c92a
Ph03niX-X <Ph03niX-X@outlook.com>
2023-03-15 16:57:03 +0200

    Remove unused import directive

--- app/src/main/java/com/ph03nix_x/capacityinfo/interfaces/BatteryInfoInterface.kt
+++ app/src/main/java/com/ph03nix_x/capacityinfo/interfaces/BatteryInfoInterface.kt
@@ -23,7 +23,6 @@ import java.io.FileReader
 import java.io.IOException
 import java.text.DecimalFormat
 import kotlin.math.pow
-import kotlin.random.Random
 
 @SuppressWarnings("PrivateApi")
 interface BatteryInfoInterface {
