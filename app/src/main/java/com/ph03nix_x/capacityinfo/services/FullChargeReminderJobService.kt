d9e81dd0e99bf9622094a01432be3983782ef0d2
Ph03niX-X <Ph03niX-X@outlook.com>
2023-07-21 07:48:29 +0300

    Fix

--- app/src/main/java/com/ph03nix_x/capacityinfo/services/FullChargeReminderJobService.kt
+++ app/src/main/java/com/ph03nix_x/capacityinfo/services/FullChargeReminderJobService.kt
@@ -19,23 +19,20 @@ class FullChargeReminderJobService : JobService(), NotificationInterface {
 
     override fun onStartJob(params: JobParameters?): Boolean {
 
-        if(CapacityInfoService.instance == null)
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+
+        val isNotifyBatteryIsFullyCharged = pref.getBoolean(IS_NOTIFY_BATTERY_IS_FULLY_CHARGED,
+            resources.getBoolean(R.bool.is_notify_battery_is_fully_charged))
+
+        val isNotifyFullyChargeReminder = pref.getBoolean(
+            IS_NOTIFY_FULL_CHARGE_REMINDER,
+            resources.getBoolean(R.bool.is_notify_full_charge_reminder_default_value))
+
+        if(!isNotifyFullyChargeReminder || CapacityInfoService.instance == null)
             ServiceHelper.cancelJob(this, Constants.IS_NOTIFY_FULL_CHARGE_REMINDER_JOB_ID)
 
-        else {
-            val pref = PreferenceManager.getDefaultSharedPreferences(this)
-
-            val isNotifyBatteryIsFullyCharged = pref.getBoolean(IS_NOTIFY_BATTERY_IS_FULLY_CHARGED,
-                resources.getBoolean(R.bool.is_notify_battery_is_fully_charged))
-
-            val isNotifyFullyChargeReminder = pref.getBoolean(
-                IS_NOTIFY_FULL_CHARGE_REMINDER,
-                resources.getBoolean(R.bool.is_notify_full_charge_reminder_default_value))
-
-            if(isNotifyBatteryIsFullyCharged && isNotifyFullyChargeReminder &&
-                CapacityInfoService.instance?.isFull == true)
-                onNotifyBatteryFullyCharged(this)
-        }
+        else if(isNotifyBatteryIsFullyCharged && CapacityInfoService.instance?.isFull == true)
+            onNotifyBatteryFullyCharged(this)
 
         return false
     }
