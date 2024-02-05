a4d0d5cc13688ffa9f5b55d82e59b25663ef121e
Ph03niX-X <Ph03niX-X@outlook.com>
2024-02-05 07:04:55 +0200

    Change Wakelock Timeout

--- app/src/main/java/com/ph03nix_x/capacityinfo/utilities/Constants.kt
+++ app/src/main/java/com/ph03nix_x/capacityinfo/utilities/Constants.kt
@@ -37,5 +37,5 @@ object Constants {
     const val CHARGING_VOLTAGE_WATT = 5.0
 
     val CHECK_PREMIUM_JOB_SERVICE_PERIODIC = 12.hours.inWholeMilliseconds
-    val SERVICE_WAKELOCK_TIMEOUT = 5.minutes.inWholeMilliseconds
+    val SERVICE_WAKELOCK_TIMEOUT = 30.minutes.inWholeMilliseconds
 }
