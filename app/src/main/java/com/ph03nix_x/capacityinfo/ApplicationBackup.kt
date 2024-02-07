1890d396cdd2eaae9948161c491a83de414b358b
Ph03niX-X <Ph03niX-X@outlook.com>
2024-02-07 06:50:25 +0200

    [Backup] Remove save Last Charge Time

--- app/src/main/java/com/ph03nix_x/capacityinfo/ApplicationBackup.kt
+++ app/src/main/java/com/ph03nix_x/capacityinfo/ApplicationBackup.kt
@@ -10,7 +10,6 @@ import com.ph03nix_x.capacityinfo.utilities.PreferencesKeys.BATTERY_LEVEL_TO
 import com.ph03nix_x.capacityinfo.utilities.PreferencesKeys.BATTERY_LEVEL_WITH
 import com.ph03nix_x.capacityinfo.utilities.PreferencesKeys.CAPACITY_ADDED
 import com.ph03nix_x.capacityinfo.utilities.PreferencesKeys.DESIGN_CAPACITY
-import com.ph03nix_x.capacityinfo.utilities.PreferencesKeys.LAST_CHARGE_TIME
 import com.ph03nix_x.capacityinfo.utilities.PreferencesKeys.NUMBER_OF_CHARGES
 import com.ph03nix_x.capacityinfo.utilities.PreferencesKeys.NUMBER_OF_CYCLES
 import com.ph03nix_x.capacityinfo.utilities.PreferencesKeys.PERCENT_ADDED
@@ -22,11 +21,8 @@ class ApplicationBackup : BackupAgent() {
     private var prefArrays: MutableMap<String, *>? = null
 
     override fun onCreate() {
-
         super.onCreate()
-
         val pref = PreferenceManager.getDefaultSharedPreferences(this)
-
         prefArrays = pref.all
     }
 
@@ -37,31 +33,21 @@ class ApplicationBackup : BackupAgent() {
                            newState: ParcelFileDescriptor?) {}
 
     override fun onRestoreFinished() {
-
         super.onRestoreFinished()
-
         val prefsTempList = arrayListOf(BATTERY_LEVEL_TO, BATTERY_LEVEL_WITH,
-            DESIGN_CAPACITY, CAPACITY_ADDED, LAST_CHARGE_TIME, PERCENT_ADDED, RESIDUAL_CAPACITY)
-
+            DESIGN_CAPACITY, CAPACITY_ADDED, PERCENT_ADDED, RESIDUAL_CAPACITY)
         prefsTempList.forEach {
             with(prefArrays) {
                 when {
-
                     this?.containsKey(it) == false -> pref?.edit()?.remove(it)?.apply()
-
                     else -> {
-
                         this?.forEach {
-
                             when(it.key) {
-
                                 NUMBER_OF_CHARGES -> pref?.edit()?.putLong(it.key,
                                     it.value as Long)?.apply()
-
-                                BATTERY_LEVEL_TO, BATTERY_LEVEL_WITH, LAST_CHARGE_TIME,
-                                DESIGN_CAPACITY, RESIDUAL_CAPACITY, PERCENT_ADDED -> pref?.edit()
+                                BATTERY_LEVEL_TO, BATTERY_LEVEL_WITH, DESIGN_CAPACITY,
+                                RESIDUAL_CAPACITY, PERCENT_ADDED -> pref?.edit()
                                     ?.putInt(it.key, it.value as Int)?.apply()
-
                                 CAPACITY_ADDED, NUMBER_OF_CYCLES -> pref?.edit()?.putFloat(it.key,
                                     it.value as Float)?.apply()
                             }
--- app/src/main/java/com/ph03nix_x/capacityinfo/interfaces/BackupSettingsInterface.kt
+++ app/src/main/java/com/ph03nix_x/capacityinfo/interfaces/BackupSettingsInterface.kt
@@ -32,21 +32,14 @@ import kotlin.time.Duration.Companion.seconds
 interface BackupSettingsInterface {
 
     fun BackupSettingsFragment.onExportSettings(intent: Intent?) {
-
         val prefPath = "${requireContext().filesDir?.parent}/shared_prefs/" +
                 "${requireContext().packageName}_preferences.xml"
         val prefName = File(prefPath).name
-
         val pref = PreferenceManager.getDefaultSharedPreferences(requireContext())
-
         val tokenPref = pref.getString(TOKEN_PREF, null)
-
         CoroutineScope(Dispatchers.IO).launch {
-
             try {
-
                 MainActivity.isOnBackPressed = false
-
                 with(pref) {
                     edit().apply {
                         if(contains(UPDATE_TEMP_SCREEN_TIME)) remove(UPDATE_TEMP_SCREEN_TIME)
@@ -54,53 +47,37 @@ interface BackupSettingsInterface {
                         apply()
                     }
                 }
-
                 val pickerDir = intent?.data?.let {
                     requireContext().let { it1 -> DocumentFile.fromTreeUri(it1, it) }
                 }
-
                 pickerDir?.findFile(prefName)?.delete()
-
                 val outputStream = pickerDir?.createFile("text/xml",
                     prefName)?.uri?.let {
                     requireContext().contentResolver?.openOutputStream(it)
                 }
-
                 val fileInputStream = FileInputStream(prefPath)
                 val buffer = byteArrayOf((1024 * 8).toByte())
                 var read: Int
-
                 while (true) {
-
                     read = fileInputStream.read(buffer)
-
                     if(read != -1)
                         outputStream?.write(buffer, 0, read)
                     else break
                 }
-
                 fileInputStream.close()
                 outputStream?.flush()
                 outputStream?.close()
-
                 withContext(Dispatchers.Main) {
-
                     MainActivity.isOnBackPressed = true
-
                     Toast.makeText(requireContext(),
                         getString(R.string.successful_export_of_settings, prefName),
                         Toast.LENGTH_LONG).show()
-
                     pref.edit().putString(TOKEN_PREF, tokenPref).apply()
                 }
             }
-
             catch(e: Exception) {
-
                 withContext(Dispatchers.Main) {
-
                     MainActivity.isOnBackPressed = true
-
                     Toast.makeText(requireContext(), getString(R.string.error_exporting_settings,
                         e.message ?: e.toString()), Toast.LENGTH_LONG).show()
                 }
@@ -109,82 +86,55 @@ interface BackupSettingsInterface {
     }
 
     fun BackupSettingsFragment.onImportSettings(uri: Uri?) {
-
         val prefPath = "${requireContext().filesDir?.parent}/shared_prefs/" +
                 "${requireContext().packageName}_preferences.xml"
-
         CoroutineScope(Dispatchers.IO).launch {
-
             try {
-
                 MainActivity.isOnBackPressed = false
-
                 withContext(Dispatchers.Main) {
-
                     Toast.makeText(requireContext(), R.string.import_settings_3dots,
                         Toast.LENGTH_LONG).show()
-
                     if(CapacityInfoService.instance != null)
                         ServiceHelper.stopService(requireContext(), CapacityInfoService::class.java)
-
                     if(OverlayService.instance != null)
                         ServiceHelper.stopService(requireContext(), OverlayService::class.java)
                 }
-
                 val pref = PreferenceManager.getDefaultSharedPreferences(requireContext())
-
                 val prefArrays: HashMap<String, Any?> = hashMapOf()
-
                 pref.all.forEach {
-
                     when(it.key) {
-
                         PreferencesKeys.BATTERY_LEVEL_TO, PreferencesKeys.BATTERY_LEVEL_WITH,
                         PreferencesKeys.DESIGN_CAPACITY, PreferencesKeys.CAPACITY_ADDED,
-                        PreferencesKeys.LAST_CHARGE_TIME, PreferencesKeys.PERCENT_ADDED,
-                        PreferencesKeys.RESIDUAL_CAPACITY -> prefArrays[it.key] = it.value
+                        PreferencesKeys.PERCENT_ADDED, PreferencesKeys.RESIDUAL_CAPACITY ->
+                            prefArrays[it.key] = it.value
                     }
                 }
-
                 delay(2.seconds)
                 if(File(prefPath).exists()) File(prefPath).delete()
-
                 File(prefPath).createNewFile()
-
                 val fileOutputStream = FileOutputStream(prefPath)
                 val inputStream = uri?.let {
                     requireContext().contentResolver?.openInputStream(it) }
-
                 val buffer = byteArrayOf((1024 * 8).toByte())
                 var read: Int
-
                 while (true) {
-
                     read = inputStream?.read(buffer) ?: -1
-
                     if(read != -1)
                         fileOutputStream.write(buffer, 0, read)
                     else break
                 }
-
                 inputStream?.close()
                 fileOutputStream.flush()
                 fileOutputStream.close()
-
                 withContext(Dispatchers.Main) {
-
                     MainActivity.isOnBackPressed = true
-
                     MainApp.restartApp(requireContext(), prefArrays)
                 }
             }
 
             catch(e: Exception) {
-
                 withContext(Dispatchers.Main) {
-
                     MainActivity.isOnBackPressed = true
-
                     Toast.makeText(requireContext(), getString(
                         R.string.error_importing_settings, e.message ?: e.toString()),
                         Toast.LENGTH_LONG).show()
@@ -194,62 +144,43 @@ interface BackupSettingsInterface {
     }
 
     fun BackupSettingsFragment.onExportHistory(intent: Intent?) {
-
         val dbPath = "${requireContext().filesDir?.parent}/databases/History.db"
         val dbName = "History.db"
-
         CoroutineScope(Dispatchers.IO).launch {
-
             try {
-
                 if(HistoryHelper.isHistoryEmpty(requireContext()))
                     throw IOException (getString(R.string.history_is_empty))
-
                 MainActivity.isOnBackPressed = false
-
                 val pickerDir = intent?.data?.let {
                     requireContext().let { it1 -> DocumentFile.fromTreeUri(it1, it) }
                 }
-
                 delay(1.seconds)
                 pickerDir?.findFile(dbName)?.delete()
                 val outputStream = pickerDir?.createFile("application/vnd.sqlite3",
                     dbName)?.uri?.let {
                     requireContext().contentResolver?.openOutputStream(it)
                 }
-
                 val fileInputStream = FileInputStream(dbPath)
                 val buffer = byteArrayOf((1024 * 8).toByte())
                 var read: Int
-
                 while (true) {
-
                     read = fileInputStream.read(buffer)
-
                     if(read != -1)
                         outputStream?.write(buffer, 0, read)
                     else break
                 }
-
                 fileInputStream.close()
                 outputStream?.flush()
                 outputStream?.close()
-
                 withContext(Dispatchers.Main) {
-
                     Toast.makeText(requireContext(), getString(
                         R.string.history_exported_successfully), Toast.LENGTH_LONG).show()
                 }
-
                 MainActivity.isOnBackPressed = true
             }
-
             catch(e: java.lang.Exception) {
-
                 withContext(Dispatchers.Main) {
-
                     MainActivity.isOnBackPressed = true
-
                     Toast.makeText(requireContext(), "${getString(R.string
                         .error_exporting_history)}\n${e.message ?: e.toString()}",
                         Toast.LENGTH_LONG).show()
@@ -259,67 +190,44 @@ interface BackupSettingsInterface {
     }
 
     fun BackupSettingsFragment.onImportHistory(uri: Uri?, exportHistoryPref: Preference?) {
-
         val dbPath = "${requireContext().filesDir?.parent}/databases/History.db"
-
         CoroutineScope(Dispatchers.IO).launch {
-
             try {
-
                 MainActivity.isOnBackPressed = false
-
                 delay(1.seconds)
                 File(dbPath).deleteOnExit()
                 File("$dbPath-journal").deleteOnExit()
-
                 File(dbPath).createNewFile()
-
                 val fileOutputStream = FileOutputStream(dbPath)
                 val inputStream = uri?.let {
                     requireContext().contentResolver?.openInputStream(it) }
-
                 val buffer = byteArrayOf((1024 * 8).toByte())
                 var read: Int
-
                 while (true) {
-
                     read = inputStream?.read(buffer) ?: -1
-
                     if(read != -1)
                         fileOutputStream.write(buffer, 0, read)
                     else break
                 }
-
                 inputStream?.close()
                 fileOutputStream.flush()
                 fileOutputStream.close()
-
                 MainActivity.isOnBackPressed = true
-
                 val isHistoryNotEmpty = HistoryHelper.isHistoryNotEmpty(requireContext())
-
                 withContext(Dispatchers.Main) {
-
                     exportHistoryPref?.isEnabled = isHistoryNotEmpty && !HistoryHelper
                         .isHistoryMax(requireContext())
                 }
-
                 if(!isHistoryNotEmpty)
                     throw IOException(getString(R.string.history_is_empty))
-
                 else withContext(Dispatchers.Main) {
-
                     Toast.makeText(requireContext(), getString(R.string
                         .history_imported_successfully), Toast.LENGTH_LONG).show()
                 }
             }
-
             catch(e: java.lang.Exception) {
-
                 withContext(Dispatchers.Main) {
-
                     MainActivity.isOnBackPressed = true
-
                     Toast.makeText(requireContext(), "${getString(R.string
                         .error_importing_history)}\n${e.message ?: e.toString()}",
                         Toast.LENGTH_LONG).show()
