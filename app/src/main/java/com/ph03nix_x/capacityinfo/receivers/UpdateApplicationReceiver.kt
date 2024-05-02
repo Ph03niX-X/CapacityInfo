bc82a98a544d003520783b716c5b3b536b30a811
Ph03niX-X <Ph03niX-X@outlook.com>
2024-05-02 14:14:23 +0300

    Remove removeOldPreferences function

--- app/src/main/java/com/ph03nix_x/capacityinfo/receivers/UpdateApplicationReceiver.kt
+++ app/src/main/java/com/ph03nix_x/capacityinfo/receivers/UpdateApplicationReceiver.kt
@@ -33,7 +33,6 @@ class UpdateApplicationReceiver : BroadcastReceiver(), PremiumInterface {
                     }
                    context.resources.getStringArray(R.array.tab_on_application_launch_values)
                     pref.edit().putString(PreferencesKeys.TAB_ON_APPLICATION_LAUNCH, "0").apply()
-                removeOldPreferences(context)
                 if(!pref.getBoolean(IS_AUTO_START_UPDATE_APP, context.resources.getBoolean(
                         R.bool.is_auto_start_update_app))) return
                 ServiceHelper.cancelAllJobs(context)
@@ -44,38 +43,6 @@ class UpdateApplicationReceiver : BroadcastReceiver(), PremiumInterface {
                 if(OverlayService.instance == null && OverlayInterface.isEnabledOverlay(context)
                     && !ServiceHelper.isStartedOverlayService())
                     ServiceHelper.startService(context, OverlayService::class.java)
-            }
-        }
-    }
-
-    @Deprecated("Will be removed in 8.0")
-    private fun removeOldPreferences(context: Context) {
-        CoroutineScope(Dispatchers.IO).launch {
-            val pref = PreferenceManager.getDefaultSharedPreferences(context)
-            arrayListOf("temperature_in_fahrenheit", "voltage_in_mv", "is_fps_overlay",
-                "is_show_faq", "is_show_donate_message", "is_show_premium_info_dialog",
-                "is_supported", "is_show_not_supported_dialog", "language",
-                "is_enable_fake_battery_wear", "fake_battery_wear_value", "is_high_battery_wear",
-                "is_very_high_battery_wear", "is_critical_battery_wear",
-                "${context.packageName}_preferences.products.cache.v2_6.version",
-                "${context.packageName}_preferences.products.cache.v2_6",
-                "${context.packageName}_preferences.products.restored.v2_6",
-                "${context.packageName}_preferences.subscriptions.cache.v2_6",
-                "${context.packageName}_preferences.subscriptions.cache.v2_6.version",
-                "is_battery_wear", "is_show_instruction", "is_show_backup_information",
-                "is_auto_backup_settings", "is_backup_settings_to_microsd",
-                "frequency_of_auto_backup_settings", "is_notify_battery_is_charged_voltage",
-                "battery_notify_charged_voltage", "is_notify_battery_is_discharged_voltage",
-                "battery_notify_discharged_voltage", "is_notify_charging_current",
-                "charging_current_level_notify", "is_notify_discharge_current",
-                "discharge_current_level_notify", "is_fast_charge_debug",
-                "is_battery_health_android_overlay", "is_last_charge_time_overlay").forEach {
-                with(pref) {
-                    edit().apply {
-                        if(contains(it)) this.remove(it)
-                        apply()
-                    }
-                }
             }
         }
     }
