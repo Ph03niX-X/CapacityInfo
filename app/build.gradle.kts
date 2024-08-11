6aa641ab722c4e80745b4b1cb1ef4a38044f834c
Ph03niX-X <Ph03niX-X@outlook.com>
2024-08-11 15:59:44 +0300

    Version 8.1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         namespace = applicationId
         minSdk = 26
         targetSdk = 35
-        versionCode = 1644
-        versionName = "8.0.0.5"
+        versionCode = 1654
+        versionName = "8.1.0.1"
         setProperty("archivesBaseName", "$appName $versionName (Build $versionCode)")
         buildConfigField("String", "BUILD_DATE", "\"${getBuildDate()}\"")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
