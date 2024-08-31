19bdccd05bb9f8c9b82b174e7aad187b41f21489
Ph03niX-X <Ph03niX-X@outlook.com>
2024-08-31 20:29:14 +0300

    Update Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 buildscript {
     extra.apply {
-        set("kotlin_version", "2.0.10")
+        set("kotlin_version", "2.0.20")
     }
     val kotlinVersion = rootProject.extra.get("kotlin_version") as String
     repositories {
