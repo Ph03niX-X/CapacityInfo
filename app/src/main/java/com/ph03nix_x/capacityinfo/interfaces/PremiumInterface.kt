d8d15c7e8ee605d5c696dbf08a4164158dd77e55
Ph03niX-X <Ph03niX-X@outlook.com>
2025-02-10 19:10:05 +0200

    Fix SQLiteDatabaseLockedException

--- app/src/main/java/com/ph03nix_x/capacityinfo/interfaces/PremiumInterface.kt
+++ app/src/main/java/com/ph03nix_x/capacityinfo/interfaces/PremiumInterface.kt
@@ -5,6 +5,7 @@ import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.database.sqlite.SQLiteDatabaseLockedException
 import android.net.Uri
 import android.view.View
 import android.widget.Toast
@@ -466,12 +467,18 @@ interface PremiumInterface: PurchasesUpdatedListener, NavigationInterface {
                 }
             }
             }
-        if(HistoryHelper.isHistoryNotEmpty(context)) HistoryHelper.clearHistory(context)
-        withContext(Dispatchers.Main) {
-            ServiceHelper.cancelJob(context,
-                Constants.IS_NOTIFY_FULL_CHARGE_REMINDER_JOB_ID)
-            if(OverlayService.instance != null)
-                ServiceHelper.stopService(context, OverlayService::class.java)
+
+        try {
+            if(HistoryHelper.isHistoryNotEmpty(context)) HistoryHelper.clearHistory(context)
+        }
+        catch (_: SQLiteDatabaseLockedException) {}
+        finally {
+            withContext(Dispatchers.Main) {
+                ServiceHelper.cancelJob(context,
+                    Constants.IS_NOTIFY_FULL_CHARGE_REMINDER_JOB_ID)
+                if(OverlayService.instance != null)
+                    ServiceHelper.stopService(context, OverlayService::class.java)
+            }
         }
     }
 }
