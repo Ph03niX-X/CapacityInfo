9549e6f11de55f718a80147458c415b98ffb9bd8
Ph03niX-X <Ph03niX-X@outlook.com>
2025-04-15 18:27:16 +0300

    Version 9.0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         namespace = applicationId
         minSdk = 26
         targetSdk = 36
-        versionCode = 1719
-        versionName = "9.0.0.4"
+        versionCode = 1721
+        versionName = "9.0.1.1"
         setProperty("archivesBaseName", "$appName $versionName (Build $versionCode)")
         buildConfigField("String", "BUILD_DATE", "\"${getBuildDate()}\"")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
