3d9a3a2b415579736fe0eb1d7f7e1bdfffb46305
Ph03niX-X <Ph03niX-X@outlook.com>
2025-05-17 08:44:10 +0300

    Refactoring

--- app/src/main/java/com/ph03nix_x/capacityinfo/interfaces/AdsInterface.kt
+++ app/src/main/java/com/ph03nix_x/capacityinfo/interfaces/AdsInterface.kt
@@ -3,7 +3,7 @@ package com.ph03nix_x.capacityinfo.interfaces
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
-import com.ph03nix_x.capacityinfo.AD_UNIT_ID
+import com.ph03nix_x.capacityinfo.R
 import com.ph03nix_x.capacityinfo.activities.MainActivity
 import com.ph03nix_x.capacityinfo.utilities.PreferencesKeys.NUMBER_OF_CHARGES
 import com.ph03nix_x.capacityinfo.utilities.PreferencesKeys.NUMBER_OF_FULL_CHARGES
@@ -16,8 +16,8 @@ import com.ph03nix_x.capacityinfo.utilities.PreferencesKeys.NUMBER_OF_FULL_CHARG
 interface AdsInterface {
 
     private fun MainActivity.loadAds() {
-        InterstitialAd.load(this, AD_UNIT_ID, AdRequest.Builder().build(),
-            object : InterstitialAdLoadCallback() {
+        InterstitialAd.load(this, resources.getString(R.string.ad_unit_id),
+            AdRequest.Builder().build(), object : InterstitialAdLoadCallback() {
                 override fun onAdLoaded(interstitialAd: InterstitialAd) {
                     interstitialAd.show(this@loadAds)
                 }
