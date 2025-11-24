05df024a80cc798ba57a87a0d54655a7bc4e9157
Ph03niX-X <Ph03niX-X@outlook.com>
2025-11-24 14:24:05 +0200

    Update androidx.core:core-ktx

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,7 @@ dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
     implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.core:core-ktx:1.17.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
